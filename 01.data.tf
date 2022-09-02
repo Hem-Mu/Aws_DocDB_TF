b472e7fb976e2b7116d9b5a470f4cf722f44a3f3
Hem-Mu <93104310+Hem-Mu@users.noreply.github.com>
2022-09-02 13:30:04 +0900

    Update 01.data.tf

--- 01.data.tf
+++ 01.data.tf
@@ -9,13 +9,13 @@
 # } # depends on S3
 terraform {
   backend "local" {
-    path = "C:/Users/김햄무/Desktop/terraform/Aws/@init_network/@docDB/terraform.tfstate"
+    path = ".././terraform.tfstate"
   } # backend 변경 전 destroy 권장
 } 
 
 data "terraform_remote_state" "network" {
     backend = "local"
     config = {
-        path = "C:/Users/김햄무/Desktop/terraform/Aws/@init_network/terraform.tfstate"
+        path = "./terraform.tfstate"
     }   
 }
